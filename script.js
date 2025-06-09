357dbbbf8db6652dfd7a466b941692bd227b660d
baljyoti321 <baljyotihetauda@gmail.com>
2025-06-09 20:52:33 +0545

    Initial commit of school website

--- script.js
+++ script.js
@@ -22,4 +22,15 @@ function toggleGallery(button) {
     ? "Show less"
     : "Show more";
 }
+// Back to Top Button
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  backToTop.style.display = window.scrollY > 200 ? "block" : "none";
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
 console.log("Script loaded!");
